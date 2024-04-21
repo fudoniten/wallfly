f079b882da988e74950eb773d842755f8665d21e
niten <niten@fudo.org>
2024-04-21 12:11:28 -0700

    Update deps with helpers

--- flake.nix
+++ flake.nix
@@ -2,36 +2,45 @@
   description = "WallFly presence monitor.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     utils.url = "github:numtide/flake-utils";
-    clj-nix = {
-      url = "github:jlesquembre/clj-nix";
+    helpers = {
+      url = "git+https://fudo.dev/public/nix-helpers.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fudo-clojure.url = "git+https://fudo.dev/public/fudo-clojure.git";
   };
 
-  outputs = { self, nixpkgs, utils, clj-nix, ... }:
+  outputs = { self, nixpkgs, utils, helpers, fudo-clojure, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
-        cljpkgs = clj-nix.packages."${system}";
-        update-deps = pkgs.writeShellScriptBin "update-deps.sh" ''
-          ${clj-nix.packages."${system}".deps-lock}/bin/deps-lock
-        '';
+        gs = import nixpkgs { inherit system; };
+
+        cljLibs = {
+          "org.fudo/fudo-clojure" =
+            fudo-clojure.packages."${system}".fudo-clojure;
+        };
       in {
         packages = {
-          wallfly = cljpkgs.mkCljBin {
+          wallfly = helpers.packages."${system}".mkClojureBin {
             projectSrc = ./.;
             name = "org.fudo/wallfly";
-            main-ns = "wallfly.core";
-            jdkRunner = pkgs.jdk17_headless;
+            primaryNamespace = "wallfly.core";
+            src = ./.;
+            inherit cljLibs;
           };
         };
 
         defaultPackage = self.packages."${system}".wallfly;
 
-        devShell =
-          pkgs.mkShell { buildInputs = with pkgs; [ clojure update-deps ]; };
+        devShells = rec {
+          default = updateDeps;
+          updateDeps = pkgs.mkShell {
+            buildInputs = with helpers.packages."${system}";
+              [ (updateClojureDeps cljLibs) ];
+          };
+        };
       }) // {
         overlay = final: prev: {
           inherit (self.packages."${prev.system}") wallfly;
